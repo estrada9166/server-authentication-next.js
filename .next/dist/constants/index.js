91fbbd6bb98bf74d18c3960d61ec888402bcf19c
alejandro estrada <estrada9166@gmail.com>
2017-09-26 20:22:04 -0500

    Middleware updated to check if is logged

--- .next/dist/constants/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var URL = exports.URL = 'http://localhost:8000';
-var LOGGED_IN = exports.LOGGED_IN = 'LOGGED_IN';
-var INPUT_VALUE = exports.INPUT_VALUE = 'INPUT_VALUE';
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbnN0YW50cy9pbmRleC5qcyJdLCJuYW1lcyI6WyJVUkwiLCJMT0dHRURfSU4iLCJJTlBVVF9WQUxVRSJdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBTyxJQUFNLG9CQUFNLEFBQVosQUFDUDtBQUFPLElBQU0sZ0NBQVksQUFBbEIsQUFDUDtBQUFPLElBQU0sb0NBQWMsQUFBcEIiLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiL1VzZXJzL2FsZWphbmRyb2VzdHJhZGEvRG9jdW1lbnRzL0NvZGUvcHJ1ZWJhcy9hdXRoZW50aWNhdGlvbi1KV1QifQ==
