e2ded8288a791eec10c2699c225b1e9b0e799b0a
Evan Burrell <burrelle@oregonstate.edu>
2018-10-04 16:19:32 -0700

    Spacing

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,31 @@
-import React from 'react'
-import {Provider} from "react-redux";
-import App, {Container} from "next/app";
-import withRedux from "next-redux-wrapper";
-import {initStore} from "../store";
+import React from 'react';
+import { Provider } from 'react-redux';
+import App, { Container } from 'next/app';
+import withRedux from 'next-redux-wrapper';
+import { initStore } from '../store';
 
-export default withRedux(initStore, {debug: true})(class MyApp extends App {
-
-    static async getInitialProps({Component, ctx}) {
-        return {
-            pageProps: {
-                // Call page-level getInitialProps
-                ...(Component.getInitialProps ? await Component.getInitialProps(ctx) : {}),
-            }
-        };
+export default withRedux(initStore, { debug: true })(
+  class MyApp extends App {
+    static async getInitialProps({ Component, ctx }) {
+      return {
+        pageProps: {
+          // Call page-level getInitialProps
+          ...(Component.getInitialProps
+            ? await Component.getInitialProps(ctx)
+            : {})
+        }
+      };
     }
 
     render() {
-        const {Component, pageProps, store} = this.props;
-        return (
-            <Container>
-                <Provider store={store}>
-                    <Component {...pageProps} />
-                </Provider>
-            </Container>
-        );
+      const { Component, pageProps, store } = this.props;
+      return (
+        <Container>
+          <Provider store={store}>
+            <Component {...pageProps} />
+          </Provider>
+        </Container>
+      );
     }
-
-});
+  }
+);
