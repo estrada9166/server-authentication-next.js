cb55297a383c194eaaa80a38397b2293b914daaa
Evan Burrell <burrelle@oregonstate.edu>
2018-10-04 15:26:39 -0700

    Upgrade dependencies and upgrade next-redux-wrapper

--- components/profile/index.js
+++ components/profile/index.js
@@ -2,21 +2,20 @@ import React, {Component} from 'react'
 import Head from 'next/head'
 import Header from '../utils/Header'
 import NavBar from '../utils/NavBar'
+import {withRouter} from 'next/router'
 
-class Main extends Component {
+export default withRouter(class Main extends Component {
   render () {
-    const { pathname } = this.props.url
+    const { pathname } = this.props.router
     return (
       <div>
         <Head>
           <title>Profile</title>
-          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossOrigin="anonymous" />
         </Head>
         <Header pathname={pathname} />
         <NavBar />
       </div>
     )
   }
-}
-
-export default Main
+})
